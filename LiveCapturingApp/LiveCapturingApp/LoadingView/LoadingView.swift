b632c7fd9369463e06cb5f55b72fe49fc27f00af
A-by-alimelon <alimelon97@gmail.com>
2020-11-28 15:22:55 +0900

    [feat]: LoadingView xib 및 파일 생성

---
+++ LiveCapturingApp/LiveCapturingApp/LoadingView/LoadingView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingView.swift
+//  LiveCapturingApp
+//
+//  Created by A on 2020/11/28.
+//
+
+import UIKit
+
+class LoadingView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
